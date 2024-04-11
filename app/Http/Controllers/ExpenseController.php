49c44b5173a5245e1c6dfab2d0b0226925340c07
Phongnit <Phongdmc69@gmail.com>
2024-04-11 16:22:32 +0700

    xây dựng backend add, get data đơn giản

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -9,57 +9,89 @@ class ExpenseController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return IlluminateHttpResponse
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $expenses = Expense::all();
+        return response()->json($expenses);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  IlluminateHttpRequest  $request
+     * @return IlluminateHttpResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'amount' => 'required',
+            'description' => 'required' //optional if you want this to be required
+        ]);
+        $expense = Expense::create($request->all());
+        return response()->json(['message'=> 'expense created', 
+        'expense' => $expense]);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  AppExpense  $expense
+     * @return IlluminateHttpResponse
      */
     public function show(Expense $expense)
     {
-        //
+        return $expense;
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  AppExpense  $expense
+     * @return IlluminateHttpResponse
      */
-    public function edit(Expense $expense)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  IlluminateHttpRequest  $request
+     * @param  AppExpense  $expense
+     * @return IlluminateHttpResponse
      */
     public function update(Request $request, Expense $expense)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'amount' => 'required',
+            'description' => 'required' //optional if you want this to be required
+        ]);
+        $expense->name = $request->name();
+        $expense->amount = $request->amount();
+        $expense->description = $request->description();
+        $expense->save();
+
+        return response()->json([
+            'message' => 'expense updated!',
+            'expense' => $expense
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  AppExpense  $expense
+     * @return IlluminateHttpResponse
      */
     public function destroy(Expense $expense)
     {
-        //
+        $expense->delete();
+        return response()->json([
+            'message' => 'expense deleted'
+        ]);
+
     }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/expenses', 'ExpenseController@index')->name('expenses.all');
+
+Route::post('/expenses', 'ExpenseController@store')->name('expenses.store');
+
+Route::get('/expenses/{expense}', 'ExpenseController@show')->name('expenses.show');
+
+Route::put('/expenses/{expense}', 'ExpenseController@update')->name('expenses.update');
+
+Route::delete('/expenses/{expense}', 'ExpenseController@destory')->name('expenses.destroy');
